43317426fcb883d7348624d3204f0af36a45ee31
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-22 16:30:54 +0530

    comments

---
+++ 01. Comments/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // Single Line Comment
+  std::cout << "Single Line Comment" << '\n';
+  /*
+    Multi Line Comment
+    As Many Line as you Want
+  */
+  std::cout << "Mutli Line Comment" << '\n';
+  std::cout << "Inline Comment" << '\n'; // Inline Comment
+  return 0;
+}
