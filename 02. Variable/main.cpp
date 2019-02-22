c5f1f22b687caa89226b5af3c03de6c8cf94e3e0
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-22 16:35:14 +0530

    variable basic

---
+++ 02. Variable/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Initialization" << '\n';
+  int x;
+  std::cout << "Assignment" << '\n';
+  x = 62;
+  std::cout << "Initialization and Assignment both at once" << '\n';
+  int y = 10;
+  return 0;
+}
