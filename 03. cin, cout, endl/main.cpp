67958e319209c9530e4c0ed900e9acf28a627155
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-22 16:41:08 +0530

    cin cout endl

---
+++ 03. cin, cout, endl/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int n;
+  std::cout << "Character Output" << '\n';
+  std::cout << "Character Input" << '\n';
+  std::cin >> n;
+  std::cout << "Character Output with End the Line" << std::endl;
+  return 0;
+}
