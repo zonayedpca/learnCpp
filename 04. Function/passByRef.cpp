162aa044ef7a4e32af4bb2f5656d451ac4f5df09
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-24 18:02:06 +0530

    pass by ref

---
+++ 04. Function/passByRef.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void willChangeTheValue(auto &x) {
+  std::cout << "Start of the Function" << '\n';
+  std::cout << "Passed value: " << x << '\n';
+  x = 20;
+  std::cout << "x is now: " << x << '\n';
+  std::cout << "End of the Function" << '\n';
+}
+
+int main() {
+  std::cout << "Pass by value" << '\n';
+  int a = 10;
+  std::cout << "a is now: " << a << '\n';
+  willChangeTheValue(a);
+  std::cout << "a afterwards: " << a << '\n';
+  return 0;
+}
