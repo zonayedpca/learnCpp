1ca5e3f38130a915075cf939a23314a9867823b1
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-24 17:40:39 +0530

    foreach loop with ref

--- 12. Control Flow/foreach.cpp
+++ 12. Control Flow/foreach.cpp
@@ -5,9 +5,13 @@ int main() {
   for(int one: arr) {
     std::cout << "Element: " << one << '\n';
   }
-  std::cout << "auto keyword" << '\n';
+  std::cout << "auto Keyword" << '\n';
   for(auto one: arr) {
     std::cout << "Element: " << one << '\n';
+  }
+  std::cout << "Using Reference" << '\n';
+  for(auto &ref: arr) {
+    std::cout << "Element: " << ref << '\n';
   }
   return 0;
 }
