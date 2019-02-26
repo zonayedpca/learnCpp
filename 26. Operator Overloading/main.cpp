91820390a08371255b7ae96a3f1f20dbb2f5292d
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-02-26 17:58:31 +0530

    operator overloading main

---
+++ 26. Operator Overloading/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Calculator {
+private:
+  int m_result;
+public:
+  Calculator(int value): m_result(value) {}
+  friend Calculator operator+ (Calculator &calc1, Calculator &calc2);
+  int getResult() {
+    return m_result;
+  }
+};
+
+Calculator operator+ (Calculator &calc1, Calculator &calc2) {
+  return Calculator(calc1.m_result + calc2.m_result);
+}
+
+int main() {
+  Calculator calc1(12);
+  Calculator calc2(8);
+  Calculator result = calc1 + calc2;
+  std::cout << "Result: " << result.getResult() << '\n';
+  return 0;
+}
